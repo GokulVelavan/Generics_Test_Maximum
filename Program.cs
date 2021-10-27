8777b3c9f10d8dbccd5f32dc840bb50bf39bc196
GokulVelavan <gokulvelavans@gmail.com>
2021-10-27 12:44:55 +0530

    welcome messge

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Test_Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find The Maximum Using Generics Method");
+            Console.ReadLine();
+        }
+    }
+}

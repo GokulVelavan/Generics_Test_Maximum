1c7c88317fd77b3a3abe06e01eb34f8330feb10d
GokulVelavan <81863053+GokulVelavan@users.noreply.github.com>
2021-10-27 13:12:45 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,9 @@ namespace Generics_Test_Maximum
 {
     class Program
     {
-        public static int Maximum_Number(int firstValue, int secondValue, int thirdValue)
+        public static int Maximum_Number(int firstValue, int secondValue, int thirdValue) //method to find the maxium number
         {
+            //using inbuild method to find maximum
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
                 firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
                 firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
